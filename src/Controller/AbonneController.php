b731612ac7f702a536076e4ef8aff7a079fef5f2
Gregoire-F <gregoire.frou@gmail.com>
2026-05-20 14:29:12 +0200

    Ajout du role de Bibliothecaire dans security avec hierarchie qui fait que l'admin a au moins les memes droits que la bibliothecaire. Les controllers ont la restriction isGranted pour sécuriser les routes. Seuls l'admin ou la bibliothecaire a accès au endpoint /livre /abonne /emprunt

--- src/Controller/AbonneController.php
+++ src/Controller/AbonneController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/abonne')]
+#[IsGranted('ROLE_BIBLIOTHECAIRE')]
 final class AbonneController extends AbstractController
 {
     #[Route(name: 'app_abonne_index', methods: ['GET'])]
--- src/Controller/Api/LivreController.php
+++ src/Controller/Api/LivreController.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/api/livres')]
+#[IsGranted('ROLE_BIBLIOTHECAIRE')]
 class LivreController extends AbstractController
 {
     public function __construct(
