6038bffcf6f552c5a3a67d432d4634bfa28748d1
Gregoire-F <gregoire.frou@gmail.com>
2026-05-20 09:53:03 +0200

    Ajout des entités Livre Abonne et Emprunt avec FK livre_id et abonne_id dans la table Emprunt. Migration réalisée dans la DB mediatheque

---
+++ src/Entity/Emprunt.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpruntRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmpruntRepository::class)]
+class Emprunt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date_emprunt = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date_retour_prevue = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $date_retour_effective = null;
+
+    #[ORM\Column]
+    private ?int $livre_id = null;
+
+     #[ORM\ManyToOne(inversedBy: 'emprunts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Livre $livre = null;
+    #[ORM\ManyToOne(inversedBy: 'emprunts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Abonne $abonne = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateEmprunt(): ?\DateTime
+    {
+        return $this->date_emprunt;
+    }
+
+    public function setDateEmprunt(\DateTime $date_emprunt): static
+    {
+        $this->date_emprunt = $date_emprunt;
+
+        return $this;
+    }
+
+    public function getDateRetourPrevue(): ?\DateTime
+    {
+        return $this->date_retour_prevue;
+    }
+
+    public function setDateRetourPrevue(\DateTime $date_retour_prevue): static
+    {
+        $this->date_retour_prevue = $date_retour_prevue;
+
+        return $this;
+    }
+
+    public function getDateRetourEffective(): ?\DateTime
+    {
+        return $this->date_retour_effective;
+    }
+
+    public function setDateRetourEffective(?\DateTime $date_retour_effective): static
+    {
+        $this->date_retour_effective = $date_retour_effective;
+
+        return $this;
+    }
+
+    public function getLivreId(): ?int
+    {
+        return $this->livre_id;
+    }
+
+    public function setLivreId(int $livre_id): static
+    {
+        $this->livre_id = $livre_id;
+
+        return $this;
+    }
+    public function getAbonne(): ?Abonne
+    {
+        return $this->abonne;
+    }
+    public function setAbonne(?Abonne $abonne): static
+    {
+        $this->abonne = $abonne;
+        return $this;
+    }
+}
