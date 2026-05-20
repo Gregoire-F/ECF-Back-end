d4e4dba54918ba24c1d0ac4c3f4c8f3a681cff63
Gregoire-F <gregoire.frou@gmail.com>
2026-05-20 16:17:52 +0200

    Ajout de la contrainte unique sur l'email dans la table abonne

--- src/Entity/Abonne.php
+++ src/Entity/Abonne.php
@@ -20,7 +20,7 @@ class Abonne
     #[ORM\Column(length: 255)]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
