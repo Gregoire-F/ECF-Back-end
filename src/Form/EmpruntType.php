f3a2ea1866806b77888614712a55f2cdfcd9d2d1
Gregoire-F <gregoire.frou@gmail.com>
2026-05-20 10:14:41 +0200

    Ajout du crud sur toutes les entity avec php bin/console make:crud

---
+++ src/Form/EmpruntType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Abonne;
+use App\Entity\Emprunt;
+use App\Entity\Livre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmpruntType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date_emprunt')
+            ->add('date_retour_prevue')
+            ->add('date_retour_effective')
+            ->add('livre', EntityType::class, [
+                'class' => Livre::class,
+                'choice_label' => 'id',
+            ])
+            ->add('abonne', EntityType::class, [
+                'class' => Abonne::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Emprunt::class,
+        ]);
+    }
+}
